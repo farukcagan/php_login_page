6ab3672f8068eba4bd530e04ca0e8d770dd870fd
Faruk Cagan <faruk.cagan@bestera.com.tr>
2023-12-10 13:40:43 +0300

    login ve register ekranları tamamlandı

--- register_process.php
+++ register_process.php
@@ -7,14 +7,14 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $email = $_POST['email'];
 
-// Veritabanına ekleme sorgusu
+// Veritabanına ekleme işlemi
 $query = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
 $result = mysqli_query($conn, $query);
 
 if ($result) {
-    echo "Üye kaydı başarıyla tamamlandı. Hoş geldiniz, $username!";
+    echo "Üye kaydı başarıyla tamamlandı. Giriş yapabilirsiniz.";
 } else {
-    echo "Üye kaydı sırasında bir hata oluştu. Lütfen tekrar deneyin.";
+    echo "Üye kaydı sırasında bir hata oluştu. Hata: " . mysqli_error($conn);
 }
 
 // Veritabanı bağlantısını kapat
